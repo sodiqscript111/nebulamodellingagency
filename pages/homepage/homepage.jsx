65406646e37cebd222238de94f3bd51d2a63b594
sodiqscript111 <simidusodiqt@gmail.com>
2025-05-21 21:53:04 +0100

    Modelling agency website with responsive animation

--- pages/homepage/homepage.jsx
+++ pages/homepage/homepage.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import './homepage.css';
+import homepageVideo from '../../src/assets/homepage.mp4';
 
 export default function HomePage() {
     return (
@@ -13,7 +14,7 @@ export default function HomePage() {
                 aria-hidden="true"
                 tabIndex={-1}
             >
-                <source src="https://www.tiktok.com/@paris_fashion_week/video/7462842167250275606?is_from_webapp=1&sender_device=pc" type="video/mp4" />
+                <source src={homepageVideo} type="video/mp4" />
                 {/* Fallback text for unsupported browsers */}
                 Your browser does not support the video tag.
             </video>
